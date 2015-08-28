43b918765e1b779c90db0694d59c6da75ca75747
Bob Rudis <bob@rudis.net>
2015-08-28 18:56:45 -0400

    CRAN r-devel flight check

--- R/docxtractr-package.r
+++ R/docxtractr-package.r
@@ -2,7 +2,7 @@
 #'
 #' Microsoft Word docx files provide an XML structure that is fairly
 #' straightforward to navigate, especially when it applies to Word tables. The
-#' docxtractr package provides tools to determine table count, table structure and
+#' docxtractr package provides tools to determine table count + table structure and
 #' extract tables from Microsoft Word docx documents.
 #'
 #' @name docxtractr
@@ -12,4 +12,5 @@
 #' @importFrom xml2 xml_find_all xml_text xml_ns xml_find_one xml_attrs
 #' @importFrom dplyr bind_rows
 #' @importFrom tools file_ext
+#' @importFrom utils download.file unzip
 NULL
