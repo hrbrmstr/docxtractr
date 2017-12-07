a04b8009af4afaabd26b854d722579429eb5f7ae
boB Rudis <bob@rud.is>
2017-12-07 05:37:47 -0500

    appveyor and more tests

--- tests/testthat/test-docxtractr.R
+++ tests/testthat/test-docxtractr.R
@@ -3,6 +3,14 @@ test_that("we can do something", {
 
   doc <- read_docx(system.file("examples/data.docx", package="docxtractr"))
 
+  x <- capture.output(print(doc))
+
+  x <- capture.output(docx_describe_tbls(doc))
+
+  expect_equal(length(docx_extract_all(doc)), 1)
+
+  expect_equal(length(docx_extract_all_tbls(doc)), 1)
+
   expect_that(doc, is_a("docx"))
   expect_that(docx_tbl_count(doc), equals(1))
   expect_that(docx_extract_tbl(doc, 1), is_a("tbl"))
@@ -21,5 +29,26 @@ test_that("we can do something", {
   expect_that(nrow(tmp_3), equals(6))
   expect_that(ncol(tmp_4), equals(3))
   expect_that(nrow(tmp_5), equals(6))
+
+  tmp_6 <- assign_colnames(tmp_5, 1)
+
+  expect_equal(colnames(tmp_6), c("Aa", "Bb", "Cc"))
+
+  cmnt <- read_docx(system.file("examples/comments.docx", package="docxtractr"))
+
+  expect_equal(docx_cmnt_count(cmnt), 3)
+
+  x <- capture.output(docx_describe_cmnts(cmnt))
+
+  x <- capture.output(print(cmnt))
+
+  expect_equal(nrow(docx_extract_all_cmnts(cmnt)), 3)
+
+  real_world <- read_docx(system.file("examples/realworld.docx", package="docxtractr"))
+  tbls <- docx_extract_all_tbls(real_world)
+  expect_equal(colnames(mcga(assign_colnames(tbls[[1]], 2))),
+               c("country", "birthrate", "death_rate", "population_growth_2005",
+                 "population_growth_2050", "relative_place_in_transition", "social_factors_1",
+                 "social_factors_2", "social_factors_3"))
 
 })
