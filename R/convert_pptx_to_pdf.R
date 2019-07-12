b1d7798926a96e96c1736e8301b1356df8f96ef8
muschellij2 <muschellij2@gmail.com>
2019-07-12 16:04:05 -0400

    now named convert_to_pdf

--- R/convert_pptx_to_pdf.R
+++ R/convert_pptx_to_pdf.R
@@ -1,7 +1,7 @@
-#' Convert a PowerPoint Document to a PDF
+#' Convert a Document (usually PowerPoint) to a PDF
 #'
 #' @md
-#' @param path path to the PowerPoint document
+#' @param path path to the document, can be PowerPoint or DOCX
 #' @param pdf_file output PDF file name.  By default, creates a PDF in the
 #'        same directory as the `path` file.
 #'        This functionality requires the use of
@@ -11,10 +11,12 @@
 #' @examples
 #' \dontrun{
 #' path = system.file("examples/ex.pptx", package="docxtractr")
-#' pdf <- convert_pptx_to_pdf(path, pdf_file = tempfile(fileext = ".pdf"))
+#' pdf <- convert_to_pdf(path, pdf_file = tempfile(fileext = ".pdf"))
+#' path = system.file("examples/data.docx", package="docxtractr")
+#' pdf_doc <- convert_to_pdf(path, pdf_file = tempfile(fileext = ".pdf"))
 #' }
-convert_pptx_to_pdf <- function(path, pdf_file = sub("[.]pptx", ".pdf", path)) {
-  stopifnot(is_pptx(path))
+convert_to_pdf <- function(path, pdf_file = sub("[.]pptx", ".pdf", path)) {
+  stopifnot(is_pptx(path) | is_doc(path) | is_docx(path))
 
   lo_assert()
   lo_path <- getOption("path_to_libreoffice")
@@ -23,7 +25,7 @@ convert_pptx_to_pdf <- function(path, pdf_file = sub("[.]pptx", ".pdf", path)) {
   # will make sub("[.]pptx", ".pdf", path) output
   # and don't want to do that in case pdf_file in other location
   cp_path = tempfile(fileext = ".pptx")
-  cp_pdf = sub("[.]pptx", ".pdf", cp_path)
+  cp_pdf = sub("[.](pptx|docx|doc)$", ".pdf", cp_path)
   file.copy(path, cp_path)
 
   if (Sys.info()["sysname"] == "Windows") {
--- tests/testthat/test-pptx-conversion.R
+++ tests/testthat/test-pptx-conversion.R
@@ -5,7 +5,18 @@ test_that("we can convert a PPTX if LibreOffice Installed", {
   }, silent = TRUE)
   if (!inherits(lp, "try-error")) {
     path <- system.file("examples/ex.pptx", package = "docxtractr")
-    pdf <- convert_pptx_to_pdf(path, pdf_file = tempfile(fileext = ".pdf"))
+    pdf <- convert_to_pdf(path, pdf_file = tempfile(fileext = ".pdf"))
+    expect_true(file.size(pdf) > 0)
+  }
+})
+
+test_that("we can convert a DOCX to PDF if LibreOffice Installed", {
+  lp = try({
+    docxtractr:::lo_find()
+  }, silent = TRUE)
+  if (!inherits(lp, "try-error")) {
+    path <- system.file("examples/data.docx", package = "docxtractr")
+    pdf <- convert_to_pdf(path, pdf_file = tempfile(fileext = ".pdf"))
     expect_true(file.size(pdf) > 0)
   }
 })
